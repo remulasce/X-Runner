92b7a00e3a03b22b6077e34a52987452e71d8652
timrighettini <tim.righettini@gmail.com>
2013-10-19 11:40:14 -0700

    Added DeltaTime movement to the ship in L2
    Also modified the ship prefab to include the script, and exposed the
    movement values for the ship

--- Assets/Resources/Scenes/Level_2/L2_Ship_Script.cs
+++ Assets/Resources/Scenes/Level_2/L2_Ship_Script.cs
@@ -4,16 +4,22 @@ using System.Collections;
 //Movement and control for the L2 TopDownShooter
 public class L2_Ship_Script : MonoBehaviour
 {
-	
-	//Maximumu speed in each direction
-	float maxSpeed = 40;
-	float acceleration = 100;
-	
-	//When we're far from maxvelocity, we add some extra force to get us going
-	float startupBoost = 40;
-	//At what speed we should stop applying the startup boost.
-	float startupCutoff = 40;
-	float frictionConstant = 8f;
+
+    [System.Serializable]
+    public class MovementData
+    {
+        //Maximumu speed in each direction
+        public float maxSpeed = 40;
+        public float acceleration = 100;
+
+        //When we're far from maxvelocity, we add some extra force to get us going
+        public float startupBoost = 40;
+        //At what speed we should stop applying the startup boost.
+        public float startupCutoff = 40;
+        public float frictionConstant = 8f;
+    }
+
+    public MovementData movement = new MovementData();
 	
 	// Use this for initialization
 	void Start ()
@@ -26,8 +32,8 @@ public class L2_Ship_Script : MonoBehaviour
 	{
 		float dx = Input.GetAxis ("Horizontal");
 		float dy = Input.GetAxis ("Vertical");
-		
-		this.rigidbody.AddForce (acceleration * new Vector3 (dx, dy, 0));
+
+        this.rigidbody.AddForce(movement.acceleration * new Vector3(dx, dy, 0) * Time.deltaTime);
 	}
 	
 	//Adds extra boost when we're accelerating from low speeds, to add
@@ -44,28 +50,32 @@ public class L2_Ship_Script : MonoBehaviour
 		float signY = Mathf.Sign (vy);
 		
 		//Tricksie absolute value
-		if (signX * vx < startupCutoff && dx != 0) {
-			this.rigidbody.AddForce (startupBoost * dx, 0, 0);
+        if (signX * vx < movement.startupCutoff && dx != 0)
+        {
+            this.rigidbody.AddForce(movement.startupBoost * dx * Time.deltaTime, 0, 0);
 		}
-		if (signY * vy < startupCutoff && dy != 0) {
-			this.rigidbody.AddForce (0, startupBoost * dy, 0);
+        if (signY * vy < movement.startupCutoff && dy != 0)
+        {
+            this.rigidbody.AddForce(0, movement.startupBoost * dy * Time.deltaTime, 0);
 		}
 	}
 	
 	//Apply a bit of zeroing friction
 	void slowDown ()
 	{
-		this.rigidbody.AddForce (-frictionConstant * this.rigidbody.velocity);
+        this.rigidbody.AddForce(-movement.frictionConstant * this.rigidbody.velocity * Time.deltaTime);
 	}
 	
 	//Enforce max speed limit
 	void limitSpeed ()
 	{
-		if (Mathf.Abs (this.rigidbody.velocity.x) > maxSpeed) {
-			this.rigidbody.velocity = new Vector3 (maxSpeed * Mathf.Sign (this.rigidbody.velocity.x), 0, 0);
+        if (Mathf.Abs(this.rigidbody.velocity.x) > movement.maxSpeed)
+        {
+            this.rigidbody.velocity = new Vector3(movement.maxSpeed * Mathf.Sign(this.rigidbody.velocity.x), 0, 0);
 		}
-		if (Mathf.Abs (this.rigidbody.velocity.y) > maxSpeed) {
-			this.rigidbody.velocity = new Vector3 (0, maxSpeed * Mathf.Sign (this.rigidbody.velocity.y), 0);
+        if (Mathf.Abs(this.rigidbody.velocity.y) > movement.maxSpeed)
+        {
+            this.rigidbody.velocity = new Vector3(0, movement.maxSpeed * Mathf.Sign(this.rigidbody.velocity.y), 0);
 		}
 	}
 	
