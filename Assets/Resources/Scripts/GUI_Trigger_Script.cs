fa02f1983c875e1097c5e52315ae01fa597bd358
timrighettini <tim.righettini@gmail.com>
2013-10-20 13:37:05 -0700

    Added in new tutorial images for the first and second levels

---
+++ Assets/Resources/Scripts/GUI_Trigger_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Trigger_Script : MonoBehaviour {
+
+    // Image has to have the tutorial script on it to work
+    public GUITexture image;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            image.GetComponent<Tutorial_GUI_Script>().setOn();
+        }
+    }
+}
---
+++ Assets/Tutorial_GUI_Script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial_GUI_Script : MonoBehaviour {
+
+    public float timeToDisappear = 1.0f;
+
+    private float startTime = 0.0f;
+
+    public bool setOnWithStart = false;
+
+	// Use this for initialization
+	void Start () {
+        if (!setOnWithStart)
+        {
+            this.gameObject.GetComponent<GUITexture>().enabled = false;
+        }
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        if (Time.time - startTime > timeToDisappear && this.enabled)
+        {
+            this.gameObject.GetComponent<GUITexture>().enabled = false;
+        }
+	}
+
+    public void setOn()
+    {
+        this.gameObject.GetComponent<GUITexture>().enabled = true;
+        startTime = Time.time;
+    }
+}
