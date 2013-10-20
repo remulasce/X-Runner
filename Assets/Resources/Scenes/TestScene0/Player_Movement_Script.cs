dfe93564f25431fcb74feede62618549b02e8f40
timrighettini <tim.righettini@gmail.com>
2013-10-19 20:52:06 -0700

    Added in jetpack particles when player flies and temp powerup texture.

--- Assets/Resources/Scenes/TestScene0/Player_Movement_Script.cs
+++ Assets/Resources/Scenes/TestScene0/Player_Movement_Script.cs
@@ -102,6 +102,11 @@ public class Player_Movement_Script : MonoBehaviour {
 			canJump = false;
             isInAir = true;
             timeWhenLastJumped = Time.time;
+
+            if (isJetPackActive)
+            {
+                this.GetComponentInChildren<ParticleSystem>().Play();
+            }
 			
 			//Application.LoadLevelAdditive("test_add_scene");
 			//Application.LoadLevel("test_add_scene");
@@ -109,6 +114,10 @@ public class Player_Movement_Script : MonoBehaviour {
 		if ((Input.GetButtonUp("Jump") || Input.GetKeyUp(KeyCode.Space)) && isJumping)
         {
             isJumping = false;
+            if (isJetPackActive)
+            {
+                this.GetComponentInChildren<ParticleSystem>().Stop();
+            }
         }
 		
 		//Jump farther if we keep the space bar held down longer.
@@ -128,6 +137,7 @@ public class Player_Movement_Script : MonoBehaviour {
         {
             return;
         }        
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             if (horizontalMovement.playerOffset > horizontalMovement.leftMovementThreshold)
@@ -206,6 +216,11 @@ public class Player_Movement_Script : MonoBehaviour {
             {
                 //print("Not in air anymore"+Time.time);
                 isInAir = false;
+            }
+
+            if (isJetPackActive)
+            {
+                this.GetComponentInChildren<ParticleSystem>().Stop();
             }
         }
 
