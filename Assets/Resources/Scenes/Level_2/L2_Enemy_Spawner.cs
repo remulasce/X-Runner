0afe911caac8287cc02ea6d9a24af9df32a1bfda
timrighettini <tim.righettini@gmail.com>
2013-10-20 17:40:46 -0700

    Added in bigger blockades, game seems to lag out with waves this big.  I'll work around it when I optimize the level.
    Also, for some reason the explosion package reimported.  Dunno why.

--- Assets/Resources/Scenes/Level_2/L2_Enemy_Spawner.cs
+++ Assets/Resources/Scenes/Level_2/L2_Enemy_Spawner.cs
@@ -69,21 +69,21 @@ public class L2_Enemy_Spawner : MonoBehaviour {
          * timeTillNextWave: Seconds
          */        
 
-        W(ft_hl(10), nb_go(0, 40, 0, 17), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 38, 0, 15), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 36, 0, 13), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 10f);
+        W(ft_hl(15), nb_go(0, 40, 0, 17), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 38, 0, 15), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 36, 0, 13), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 13f);
 
-        W(ft_hl(10), nb_go(0, 40, 0, 17), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 38, 0, 15), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 36, 0, 13), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 34, 0, 11), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 10f);
+        W(ft_hl(15), nb_go(0, 40, 0, 17), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 38, 0, 15), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 36, 0, 13), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 34, 0, 11), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 12f);
         
 
-        W(ft_hl(10), nb_go(0, 40, 0, 17), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 38, 0, 15), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 36, 0, 13), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 34, 0, 11), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(10), nb_go(0, 32, 0, 9),  lb_no(), at_hm(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 40, 0, 17), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 38, 0, 15), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 36, 0, 13), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 34, 0, 11), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
+        W(ft_hl(15), nb_go(0, 32, 0, 9),  lb_no(), at_hm(10.0f), xt_no(), xb_no(), 0f);
 
 		print ("Done making spawn list");
 	}
