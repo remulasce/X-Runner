08e9db611c6a9eebadaf359df5a9ec76ace50a8f
timrighettini <tim.righettini@gmail.com>
2013-10-30 20:43:16 -0700

    Fixed a bug where delaying the elite final boss in the W call resulted in some strange behavior.

--- Assets/Resources/Scenes/Level_2/L2_Enemy_Spawner.cs
+++ Assets/Resources/Scenes/Level_2/L2_Enemy_Spawner.cs
@@ -75,65 +75,65 @@ public class L2_Enemy_Spawner : MonoBehaviour {
 
         //E(EliteBehavior.Test);
         // Scout Ship
-        W(ft_hl(1), nb_go(0, 25, 0, 0), lb_no(), at_no(), xt_im(), xb_go(45, 0), 6f);
+        //W(ft_hl(1), nb_go(0, 25, 0, 0), lb_no(), at_no(), xt_im(), xb_go(45, 0), 6f);
 
-        //Elite makes a pass at you
-        E(EliteBehavior.QuickPass);
+        ////Elite makes a pass at you
+        //E(EliteBehavior.QuickPass);
 
-        // Inital Fighter Wave
-        W(ft_hl(5), nb_go(-10, 15, 0, 0), lb_no(), at_ld(7.0f), xt_im(), xb_go(45, 0), 1f);
-        W(ft_hl(5), nb_go(10, 15, 0, 0), lb_no(), at_ld(7.0f), xt_im(), xb_go(-45, 0), 6.5f);
+        //// Inital Fighter Wave
+        //W(ft_hl(5), nb_go(-10, 15, 0, 0), lb_no(), at_ld(7.0f), xt_im(), xb_go(45, 0), 1f);
+        //W(ft_hl(5), nb_go(10, 15, 0, 0), lb_no(), at_ld(7.0f), xt_im(), xb_go(-45, 0), 6.5f);
 
-        // First Blockade
-        W(ft_hl(15), nb_go(0, 30, 0, 10), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(13), nb_go(0, 28, 0, 8), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(11), nb_go(0, 26, 0, 6), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
-        E(EliteBehavior.HangBehind);
-
-
-        // Second Fighter Wave
-        W(ft_hl(3), nb_go(15, -15, 0, 5), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), -1f);
-        W(ft_hl(3), nb_go(-15, -15, 0, 7.5f), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), 3f);
-
-        W(ft_hl(3), nb_go(-15, 15, 0, -5), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), 0f);
-        W(ft_hl(3), nb_go(15, 15, 0, -7.5f), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), 1.5f);
-        E(EliteBehavior.QuickPass);
+        //// First Blockade
+        //W(ft_hl(15), nb_go(0, 30, 0, 10), lb_no(), at_lt(10.0f), xt_no(), xb_no(), 0f);
+        //W(ft_hl(13), nb_go(0, 28, 0, 8), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        //W(ft_hl(11), nb_go(0, 26, 0, 6), lb_no(), at_ld(10.0f), xt_no(), xb_no(), 0f);
+        //E(EliteBehavior.HangBehind);
 
 
-        // Third Fighter Wave
-        W(ft_hl(3), nb_go(25, 8, 0, 2), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
-        W(ft_hl(3), nb_go(25, 8, 0, -2), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
-        W(ft_hl(3), nb_go(-25, 8, 5, 0), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
-        W(ft_hl(3), nb_go(-25, 8, -5, 0), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
+        //// Second Fighter Wave
+        //W(ft_hl(3), nb_go(15, -15, 0, 5), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), -1f);
+        //W(ft_hl(3), nb_go(-15, -15, 0, 7.5f), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), 3f);
 
-        // Second Blockade
-        W(ft_hl(15), nb_go(0, 30, 0, 10), lb_no(), at_lt(7.0f), xt_no(), xb_no(), -.01f);
-        W(ft_hl(15), nb_go(0, 28, 0, 8), lb_no(), at_lt(7.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(13), nb_go(0, 26, 0, 6), lb_no(), at_ld(7.0f), xt_no(), xb_no(), 0f);
-        W(ft_hl(11), nb_go(0, 24, 0, 4), lb_no(), at_ld(7.0f), xt_no(), xb_no(), 0f);
+        //W(ft_hl(3), nb_go(-15, 15, 0, -5), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), 0f);
+        //W(ft_hl(3), nb_go(15, 15, 0, -7.5f), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_go(45, 0), 1.5f);
+        //E(EliteBehavior.QuickPass);
 
-        // Begin spawning the cinematic asteroids (no physics, background stuff).
-        W(ft_ac());
 
-        // Giant Vertical Wave
-        W(ft_hl(15), nb_go(0, 40, 0, -60), lb_no(), at_no(), xt_im(), xb_go(0, -60), -.01f);
-        W(ft_hl(15), nb_go(0, 38, 0, -62), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 36, 0, -64), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 34, 0, -66), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 32, 0, -68), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 30, 0, -70), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 28, 0, -72), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 26, 0, -74), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 24, 0, -76), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 22, 0, -78), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 20, 0, -80), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 18, 0, -82), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 16, 0, -84), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
-        W(ft_hl(15), nb_go(0, 14, 0, -86), lb_no(), at_no(), xt_im(), xb_go(0, -60), 8f);
+        //// Third Fighter Wave
+        //W(ft_hl(3), nb_go(25, 8, 0, 2), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
+        //W(ft_hl(3), nb_go(25, 8, 0, -2), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
+        //W(ft_hl(3), nb_go(-25, 8, 5, 0), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
+        //W(ft_hl(3), nb_go(-25, 8, -5, 0), lb_no(), at_hm(6.0f), xt_tm(3.0f), xb_no(), 0f);
 
-        // Filler Wave 1
-        W(ft_hl(4), nb_go(15, 15, 0, 5), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_no(), 0f);
-        W(ft_hl(4), nb_go(-15, 15, 0, 7.5f), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_no(), 1.5f);
+        //// Second Blockade
+        //W(ft_hl(15), nb_go(0, 30, 0, 10), lb_no(), at_lt(7.0f), xt_no(), xb_no(), -.01f);
+        //W(ft_hl(15), nb_go(0, 28, 0, 8), lb_no(), at_lt(7.0f), xt_no(), xb_no(), 0f);
+        //W(ft_hl(13), nb_go(0, 26, 0, 6), lb_no(), at_ld(7.0f), xt_no(), xb_no(), 0f);
+        //W(ft_hl(11), nb_go(0, 24, 0, 4), lb_no(), at_ld(7.0f), xt_no(), xb_no(), 0f);
+
+        //// Begin spawning the cinematic asteroids (no physics, background stuff).
+        //W(ft_ac());
+
+        //// Giant Vertical Wave
+        //W(ft_hl(15), nb_go(0, 40, 0, -60), lb_no(), at_no(), xt_im(), xb_go(0, -60), -.01f);
+        //W(ft_hl(15), nb_go(0, 38, 0, -62), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 36, 0, -64), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 34, 0, -66), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 32, 0, -68), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 30, 0, -70), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 28, 0, -72), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 26, 0, -74), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 24, 0, -76), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 22, 0, -78), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 20, 0, -80), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 18, 0, -82), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 16, 0, -84), lb_no(), at_no(), xt_im(), xb_go(0, -60), 0f);
+        //W(ft_hl(15), nb_go(0, 14, 0, -86), lb_no(), at_no(), xt_im(), xb_go(0, -60), 8f);
+
+        //// Filler Wave 1
+        //W(ft_hl(4), nb_go(15, 15, 0, 5), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_no(), 0f);
+        //W(ft_hl(4), nb_go(-15, 15, 0, 7.5f), lb_no(), at_lt(15.0f), xt_tm(3.0f), xb_no(), 1.5f);
 
         //Elite quick visit
         E(EliteBehavior.PreFinalBattle);        
@@ -153,6 +153,9 @@ public class L2_Enemy_Spawner : MonoBehaviour {
         W(ft_hl(15), nb_go(0, 30, 0, 10), lb_no(), at_lt(15.0f), xt_no(), xb_no(), 0f);
         W(ft_hl(15), nb_go(0, 28, 0, 8), lb_no(), at_hm(15.0f), xt_no(), xb_no(), 0f);
         W(ft_hl(15), nb_go(0, 26, 0, 6), lb_no(), at_hm(15.0f), xt_no(), xb_no(), 0f);
+
+        // SUPER HACK ALERT -- this is done to block the elite coming in until all of the ships from the final blockade are destroyed
+        W(ft_hl(1), nb_go(0, 2600, 0, 6), lb_no(), at_hm(15.0f), xt_no(), xb_no(), -0.1f);
 
         //Elite comes and stays for real.
         E(EliteBehavior.FinalBattle);
@@ -200,7 +203,7 @@ public class L2_Enemy_Spawner : MonoBehaviour {
             W(ft_ef(), nb_go(-20, 2, 20, 2), lb_no(), at_ld(0.35f), xt_no(), xb_no(), 0);            
             break;
 		case EliteBehavior.FinalBattle:
-			W (ft_ef(), nb_go (0, 20, 0, 4), lb_lz(-13, 4, 13, 4, 2.0f)/*lb_no()*/, at_hm(4), xt_no (), xb_no (), -0.0001f);            
+			W (ft_ef(), nb_go (0, 20, 0, 4), lb_lz(-13, 4, 13, 4, 2.0f)/*lb_no()*/, at_hm(4), xt_no (), xb_no (), 0);            
 			break;
 		case EliteBehavior.Test:
 			W (ft_eb(), nb_go (-15, 12, -12, 12), lb_wp (new float[] { -12, 10, 12, 10, -12, -2, 12, -2, -12, 10 }, 5f), 
