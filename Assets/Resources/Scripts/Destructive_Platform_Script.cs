670acc954c94311fc16485c2909519bc2a60341b
timrighettini <tim.righettini@gmail.com>
2013-11-17 18:52:18 -0800

    Did some more balancing to L3.

--- Assets/Resources/Scripts/Destructive_Platform_Script.cs
+++ Assets/Resources/Scripts/Destructive_Platform_Script.cs
@@ -61,7 +61,9 @@ public class Destructive_Platform_Script : MonoBehaviour {
                 postActivationParticleSystem.particleSystem.Play();
             }
         }
-
-        audio.Play();
+        if (audio)
+        {
+            audio.Play();
+        }
     }
 }
