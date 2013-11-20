8b24990e6c6b42297e24214423c162ffeb0c7174
timrighettini <tim.righettini@gmail.com>
2013-11-20 13:23:44 -0800

    Made a change to the scene architecture in an attempt to fix a bug where multiple audio objects were created in the first scene after win.

---
+++ Assets/Resources/Scenes/Level_0/Advance_To_Title_Script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Advance_To_Title_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void LateUpdate()
+    {
+        //Application.LoadLevel("Level_4_Boss");
+        Application.LoadLevel("Title_Screen");
+    }
+}
