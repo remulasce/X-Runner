4ddb840eabc923318329157e168e5944070cbf82
Fintan O'Grady <jtkirker@gmail.com>
2013-11-22 14:15:33 -0800

    Better L1-2 transition

--- Assets/Resources/Scenes/Level_1/L1_Transition_Script.cs
+++ Assets/Resources/Scenes/Level_1/L1_Transition_Script.cs
@@ -32,5 +32,34 @@ public class L1_Transition_Script : MonoBehaviour {
 		Application.LoadLevel("Level_2_TDS");
 		//Application.LoadLevel("Level_4_Boss");
 	}
+	public void onStartCloud()
+	{
+		StartCoroutine("CloudSpawn");
+	}
+	IEnumerator CloudSpawn()
+	{
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,-10), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(1f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,1), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.5f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,10), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.5f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,1), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.5f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,4), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.25f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,15), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.25f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,4), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.25f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,15), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		yield return new WaitForSeconds(.25f);
+		Instantiate(Resources.Load("Prefabs/Level_1/Cloud"), fakeShip.transform.position-new Vector3(0,-40,10), Quaternion.AngleAxis(270, new Vector3(1,0,0)));
+		
+		
+		
+		
+		yield break;
+	}
 	
 }
