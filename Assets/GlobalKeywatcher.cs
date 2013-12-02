94b4a08b4f719485e23bb995911debf95ffb80b9
Fintan O'Grady <jtkirker@gmail.com>
2013-12-02 14:01:56 -0800

    Keywatcher in L0 to quit upon escape key

---
+++ Assets/GlobalKeywatcher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalKeywatcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		if (Input.GetKeyDown(KeyCode.BackQuote))
+		{
+			//Application.LoadLevel(Application.loadedLevelName);
+		}
+		
+	}
+}
